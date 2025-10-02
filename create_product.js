0d60ec12e172f714f2ad7eaeaab981ef8fb7e4ec
youssef <youssef.charroud445@gmail.com>
2025-10-02 16:44:37 +0100

    setup stripe

---
+++ create_product.js
@@ -0,0 +1,6 @@
+import Stripe from "stripe";
+const stripe = new Stripe(
+  "sk_test_51SD9qCAPAcVB9zmrrAL5nZMBYeMKgdYJgWA9rjUpnjB4fuX0BgnvYLEUj4yFjv37nJc2N4jGCmtjHabi8IcMjbLU00NZkgRqDz"
+);
+
+export default stripe;
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import helmet from "helmet";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-
+import stripe from "./create_product.js";
 // Load environment variables
 dotenv.config();
 
@@ -17,8 +17,39 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/create-checkout-session", async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name: "T-shirt",
+          },
+          unit_amount: 2000,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: "payment",
+    success_url: "http://localhost:4000/suc",
+    cancel_url: "http://localhost:4000/fai",
+  });
+
+  res.redirect(303, session.url);
+});
+
+app.get("/suc", (req, res) => {
+  res.send("success");
+});
+app.get("/fai", (req, res) => {
+  res.send("failed");
+});
 
 // Routes
+
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to my Express backend!" });
 });
@@ -38,4 +69,3 @@ app.use((err, req, res, next) => {
 const server = app.listen(PORT, () => {
   console.log(`✅ Server is running on http://localhost:${PORT}`);
 });
-
